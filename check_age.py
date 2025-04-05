01af023092fd408211b290352fefc5a2a336fb03
suhana0709 <farjanajaman1975@gmail.com>
2025-04-05 10:06:32 -0500

    Add files via upload

---
+++ check_age.py
@@ -0,0 +1,5 @@
+age = int(input("Please enter your age: "))
+if age>10 and age<20:
+    print("Your age is between 10 and 20.")
+else:
+    print("You are not within the age limit which is between 10 and 20")
