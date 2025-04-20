85cdb649f342b13628e5e1c5759349145cad2152
suhana0709 <farjanajaman1975@gmail.com>
2025-04-20 10:41:43 -0500

    Add files via upload

---
+++ (turtle)_square.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.Screen().bgcolor("pink")
+turtle.Screen().setup(300, 400)
+sq = turtle.Turtle()
+
+sq.forward(100)
+sq.right(90)
+sq.forward(100)
+
+sq.right(90)
+sq.forward(100)
+
+sq.right(90)
+sq.forward(100)
+
+turtle.done()
